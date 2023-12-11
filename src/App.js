f5de642828e489ff2756fe3ebd33aa2836d993ad
Akumaa <sandeep456khadka@gmail.com>
2023-12-11 23:31:31 +0545

    added some new files and css

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
       <BrowserRouter>
         <Nav />
         <Rout />
+
         <Footer />
       </BrowserRouter>
     </div>
--- src/components/homeproduct.js
+++ src/components/homeproduct.js
@@ -1,48 +1,32 @@
-// const Homeproduct = [
-//   {
-//     id: 1,
-//     Title: "Ipad pro 2022 14inch M2 chip  256Gb.",
-//     Cat: "Tablet",
-//     Price: "$2400",
-//     Img: "./images/",
-//   },
-//   {
-//     id: 1,
-//     Title: "Ipad pro 2022 14inch M2 chip  256Gb.",
-//     Cat: "Tablet",
-//     Price: "$2400",
-//     Img: "./images/",
-//   },
-//   {
-//     id: 1,
-//     Title: "Ipad pro 2022 14inch M2 chip  256Gb.",
-//     Cat: "Tablet",
-//     Price: "$2400",
-//     Img: "./images/",
-//   },
-//   {
-//     id: 1,
-//     Title: "Ipad pro 2022 14inch M2 chip  256Gb.",
-//     Cat: "Tablet",
-//     Price: "$2400",
-//     Img: "./images/",
-//   },
-// ];
+const Product = [
+  {
+    id: 1,
+    Title: "Ipad pro 2022 14inch M2 chip  256Gb.",
+    Cat: "Tablet",
+    Price: "$2400",
+    Img: "./images/product1.png",
+  },
+  {
+    id: 1,
+    Title: "Ipad pro 2022 14inch M2 chip  256Gb.",
+    Cat: "Tablet",
+    Price: "$2400",
+    Img: "./images/product2.png",
+  },
+  {
+    id: 1,
+    Title: "Ipad pro 2022 14inch M2 chip  256Gb.",
+    Cat: "Tablet",
+    Price: "$2400",
+    Img: "./images/product3.png",
+  },
+  {
+    id: 1,
+    Title: "Ipad pro 2022 14inch M2 chip  256Gb.",
+    Cat: "Tablet",
+    Price: "$2400",
+    Img: "./images/product4.png",
+  },
+];
 
-// export default Homeproduct;
-
-import React from "react";
-
-const Homeproduct = () => {
-  return (
-    <>
-      <div className="products">
-        <div className="product-container">
-          <div className="image-box"></div>
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default Homeproduct;
+export default Product;
